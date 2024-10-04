acb751950cd2e3e2d6e3a9af71357779f92a7950
OphieOphieOphie <183608652+OphieOphieOphie@users.noreply.github.com>
2024-10-04 22:14:33 +0100

    updated game state handling: exit, screen display

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import pygame
+import constants
 
 def main():
 	print("Starting asteroids!")
+	print(f"Screen width: {constants.SCREEN_WIDTH}")
+	print(f"Screen height: {constants.SCREEN_HEIGHT}")
+	pygame.init()
+	start_flag = True
+	screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+	while start_flag:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				start_flag = False
+		pygame.Surface.fill(screen, (0,0,0))
+		pygame.display.flip()
 
 if __name__ == "__main__":
 	main()
