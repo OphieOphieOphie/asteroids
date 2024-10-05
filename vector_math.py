29a0e22d94aa8e7ae2d410f37e527322556e5797
OphieOphieOphie <183608652+OphieOphieOphie@users.noreply.github.com>
2024-10-05 05:41:35 +0100

    minor alteration to vector_math.py, no change in functionality

--- vector_math.py
+++ vector_math.py
@@ -12,9 +12,6 @@ def final_velocities(v1, v2, r1, r2, m1, m2):
     def vector_subtract(v1, v2):
         return [v1[0] - v2[0], v1[1] - v2[1]]
 
-    def vector_add(v1, v2):
-        return [v1[0] + v2[0], v1[1] + v2[1]]
-
     # relative position and velocity vectors
     r_rel = vector_subtract(r1, r2)
     v_rel = vector_subtract(v1, v2)
